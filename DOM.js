611ab7fdf5eb88b7ca95c201d8da100f0548ed7f
Sebastian Opiyo <sebamopale@gmail.com>
2019-09-09 21:08:51 +0300

    Add DOM Functions

--- DOM.js
+++ DOM.js
@@ -4,6 +4,31 @@
 // We Start with the DOM.
 // Is made up of objects.
 
+//  LESSON I : What is the Dom.
+
 // To inspect the DOM
 console.log(document);
 document.bgColor = "red";
+
+//  LESSON II : DOM Functions
+
+// creates a paragraph
+var paragraph = document.createElement('P');
+
+// Add text to a paragraph
+paragraph.textContent = 'I must know how it all works.';
+
+// add test to body
+document.body.appendChild(paragraph);
+
+//  LESSON III : Event Listeners
+//Event Listeners
+var div = document.createElement('DIV');
+
+//Set height of Element
+document.body.appendChild(div);
+div.addEventListener('mousemove', function(event) {
+    console.log(event);
+})
+
+// What is a call back function.
